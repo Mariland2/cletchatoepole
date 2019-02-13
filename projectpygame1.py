346b870496117c461b1a4ecd3b637fc2140d150c
Kirill <45314516+Mariland2@users.noreply.github.com>
2019-02-13 18:55:30 +0300

    2 задача (бегающий герой)картинка

--- projectpygame1.py
+++ projectpygame1.py
@@ -1,10 +1,6 @@
-import pygame
+import pygame, random
 import os
 import sys
-
-pygame.init()
-size = width, height = 800, 800
-screen = pygame.display.set_mode(size, )
 
 
 def load_image(nich, colorkey=None):
@@ -22,17 +18,149 @@ def load_image(nich, colorkey=None):
     return image
 
 
+pygame.init()
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size, )
+all_sprites = pygame.sprite.Group()
+
+
 def draw(summa):
-    font = pygame.font.SysFont('CalNeulandShadow-Bold', 50)
-    text = font.render("На балансе:   " + str(summa//1), 1, (255, 20, 147))
+    font = pygame.font.SysFont('Magneto', 25)
+    text = font.render("On your balance:   " + str(summa // 1), 1, (0, 0, 0))
     text_x = 50
     text_y = 50
     screen.blit(text, (text_x, text_y))
 
 
-summa = 0
-cl = 10
-price = 500
+def cn1(price):
+    font1 = pygame.font.SysFont('Magneto', 15)
+    text1 = font1.render("price " + str(price // 1), 1, (0, 0, 0))
+    text_x1 = 106
+    text_y1 = 695
+    screen.blit(text1, (text_x1, text_y1))
+
+
+def cn2(price2):
+    font2 = pygame.font.SysFont('Magneto', 15)
+    text2 = font2.render("price " + str(price2 // 1), 1, (0, 0, 0))
+    text_x2 = 306
+    text_y2 = 695
+    screen.blit(text2, (text_x2, text_y2))
+
+
+def cn3(price3):
+    font3 = pygame.font.SysFont('Magneto', 15)
+    text3 = font3.render("price " + str(price3 // 1), 1, (0, 0, 0))
+    text_x3 = 506
+    text_y3 = 695
+    screen.blit(text3, (text_x3, text_y3))
+
+
+def menu():
+    screen.blit(ostrova, (0, 0))
+    font = pygame.font.SysFont('Magneto', 50)
+    text1 = font.render("Press TAB to continue", 1, (255, 20, 147))
+    text_x = 50
+    text_y = 250
+    screen.blit(text1, (text_x, text_y))
+
+    text = font.render("Press SHIFT to start again", 1, (255, 20, 147))
+    text_x2 = 50
+    text_y2 = 50
+    screen.blit(text, (text_x2, text_y2))
+
+
+class es(pygame.sprite.Sprite):
+    image = load_image("pro.png")
+    image = pygame.transform.scale(image, (100, 100))
+
+    def __init__(self, group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(group)
+        self.image = sprite.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width)
+        self.rect.y = random.randrange(height)
+
+    def update(self):
+        self.rect = self.rect.move(random.randrange(3) - 1,
+                                   random.randrange(3) - 1)
+
+
+def maing():
+    while running:
+
+        screen.blit(fonn, (0, 0))
+        screen.blit(qwe, (100, 700))
+        screen.blit(qwe, (300, 700))
+        screen.blit(qwe, (500, 700))
+        draw(summa)
+        for event in pygame.event.get():
+            # screen.blit(fonn, (0, 0))
+            # screen.blit(qwe, (100, 700))
+            # draw(summa)
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    summa += cl
+                    screen.blit(image2, (x + 50, y + 50))
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+                if event.key == pygame.K_1:
+                    if summa - price >= 0:
+                        cl += 100
+                        summa = summa - price
+                        price = price * 1.5
+                        screen.blit(pygame.transform.scale(qwe, (75, 75)), (100, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+                if event.key == pygame.K_2:
+                    if summa - price >= 0:
+                        cl += 100
+                        summa = summa - price
+                        price = price * 1.5
+                        screen.blit(pygame.transform.scale(qwe, (75, 75)), (300, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+                if event.key == pygame.K_3:
+                    if summa - price >= 0:
+                        cl += 100
+                        summa = summa - price
+                        price = price * 1.5
+                        screen.blit(pygame.transform.scale(qwe, (75, 75)), (500, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RETURN:
+                    screen.blit(image, (x, y))
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+                if event.key == pygame.K_1:
+                    if summa - price >= 0:
+                        screen.blit(qwe, (100, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+                if event.key == pygame.K_1:
+                    if summa - price >= 0:
+                        screen.blit(qwe, (300, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+                if event.key == pygame.K_1:
+                    if summa - price >= 0:
+                        screen.blit(qwe, (500, 700))
+                        pygame.display.flip()
+                        pygame.time.delay(65)
+
+        screen.blit(fonn, (0, 0))
+        draw(summa)
+        screen.blit(image, (x, y))
+        screen.blit(qwe, (100, 700))
+        pygame.display.flip()
+
+
 x, y = 300, 300
 image = load_image("pro.png")
 image = pygame.transform.scale(image, (300, 300))
@@ -46,24 +174,93 @@ fonn = pygame.transform.scale(fonn, (800, 800))
 qwe = load_image("1.png")
 qwe = pygame.transform.scale(qwe, (100, 100))
 screen.blit(qwe, (100, 700))
+screen.blit(qwe, (300, 700))
+screen.blit(qwe, (500, 700))
+
+ostrova = load_image("os.jpg")
+ostrova = pygame.transform.scale(ostrova, (800, 800))
 
 o = False
 pygame.mouse.set_visible(o)
 
 running = True
+per = []
+mrun = True
+while mrun:
+    menu()
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_TAB:
+                f = open("sohr.txt", mode="r")
+                stre = f.read()
+                stre = stre.split(' ')
+                summa = float(stre[0])
+                cl = float(stre[1])
+                price = float(stre[2])
+                price2 = float(stre[3])
+                price3 = float(stre[4])
+                cl1 = float(stre[5])
+                n = int(stre[6])
+
+                mrun = False
+            if event.key == pygame.K_LSHIFT:
+                summa = 0
+                cl = 10
+                price = 500
+                price2 = 5000
+                price3 = 50000
+                cl1 = 0
+                n = 0
+                per = []
+                mrun = False
+clock = pygame.time.Clock()
+tm = 0
 while running:
+
     screen.blit(fonn, (0, 0))
+    all_sprites.draw(screen)
     screen.blit(qwe, (100, 700))
+    screen.blit(qwe, (300, 700))
+    screen.blit(qwe, (500, 700))
     draw(summa)
+    cn1(price)
+    cn2(price2)
+    cn3(price3)
+    clock.tick(30)
+
     for event in pygame.event.get():
         # screen.blit(fonn, (0, 0))
         # screen.blit(qwe, (100, 700))
         # draw(summa)
         if event.type == pygame.QUIT:
+            f = open("sohr.txt", mode="w")
+            per.append(str(summa))
+            per.append(str(cl))
+            per.append(str(price))
+            per.append(str(price2))
+            per.append(str(price3))
+            per.append(str(cl1))
+            per.append(str(n))
+            f.write(' '.join(per))
+            f.close()
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
                 summa += cl
+                screen.blit(fonn, (0, 0))
+                draw(summa)
+                all_sprites.draw(screen)
+
+                screen.blit(qwe, (100, 700))
+                screen.blit(qwe, (300, 700))
+                screen.blit(qwe, (500, 700))
+                cn1(price)
+                cn2(price2)
+                cn3(price3)
                 screen.blit(image2, (x + 50, y + 50))
                 pygame.display.flip()
                 pygame.time.delay(65)
@@ -72,27 +269,141 @@ while running:
                     cl += 100
                     summa = summa - price
                     price = price * 1.5
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
+                    screen.blit(qwe, (300, 700))
+                    screen.blit(qwe, (500, 700))
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
                     screen.blit(pygame.transform.scale(qwe, (75, 75)), (100, 700))
                     pygame.display.flip()
                     pygame.time.delay(65)
+            if event.key == pygame.K_2:
+                if summa - price2 >= 0:
+                    cl *= 2
+                    n += 1
+                    summa = summa - price2
+                    price2 = price2 * 1.5
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
 
-            if pygame.key.get_pressed()[pygame.K_LEFT]:
-                x -= 10
+                    for i in range(n):
+                        # можно сразу создавать спрайты с указанием группы
 
-            if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                x += 10
+                        sprite = pygame.sprite.Sprite(all_sprites)
+                        # определим его вид
+                        sprite.image = load_image("pro.png")
+                        sprite.image = pygame.transform.scale(sprite.image, (100, 100))
+                        # и размеры
+                        sprite.rect = sprite.image.get_rect()
+                        # добавим спрайт в группу
+                        # all_sprites.add(sprite)
+
+                        # задаём случайное местоположение бомбочке
+                        sprite.rect.x = random.randrange(width)
+                        sprite.rect.y = random.randrange(height)
+
+                    screen.blit(qwe, (100, 700))
+                    screen.blit(qwe, (500, 700))
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
+                    screen.blit(pygame.transform.scale(qwe, (75, 75)), (300, 700))
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+            if event.key == pygame.K_3:
+                if summa - price3 >= 0:
+                    cl1 += 100
+                    summa = summa - price3
+                    price3 = price3 * 1.5
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
+
+                    screen.blit(qwe, (100, 700))
+                    screen.blit(qwe, (300, 700))
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
+                    screen.blit(pygame.transform.scale(qwe, (75, 75)), (500, 700))
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RETURN:
+                screen.blit(fonn, (0, 0))
+                draw(summa)
+                all_sprites.draw(screen)
+
+                screen.blit(qwe, (100, 700))
+                screen.blit(qwe, (300, 700))
+                screen.blit(qwe, (500, 700))
                 screen.blit(image, (x, y))
+                cn1(price)
+                cn2(price2)
+                cn3(price3)
                 pygame.display.flip()
                 pygame.time.delay(65)
             if event.key == pygame.K_1:
-                screen.blit(qwe, (100, 700))
-                pygame.display.flip()
-                pygame.time.delay(65)
+                if summa - price >= 0:
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
+
+                    screen.blit(qwe, (100, 700))
+                    screen.blit(qwe, (300, 700))
+                    screen.blit(qwe, (500, 700))
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+            if event.key == pygame.K_2:
+                if summa - price >= 0:
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
+
+                    screen.blit(qwe, (100, 700))
+                    screen.blit(qwe, (300, 700))
+                    screen.blit(qwe, (500, 700))
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
+                    pygame.display.flip()
+                    pygame.time.delay(65)
+            if event.key == pygame.K_3:
+                if summa - price >= 0:
+                    screen.blit(fonn, (0, 0))
+                    draw(summa)
+                    all_sprites.draw(screen)
+                    screen.blit(qwe, (100, 700))
+                    screen.blit(qwe, (300, 700))
+                    screen.blit(qwe, (500, 700))
+
+                    cn1(price)
+                    cn2(price2)
+                    cn3(price3)
+                    pygame.display.flip()
+                    pygame.time.delay(65)
 
     screen.blit(fonn, (0, 0))
     draw(summa)
+    all_sprites.draw(screen)
+
     screen.blit(image, (x, y))
     screen.blit(qwe, (100, 700))
+    screen.blit(qwe, (300, 700))
+    screen.blit(qwe, (500, 700))
+
+    cn1(price)
+    cn2(price2)
+    cn3(price3)
+    tm += 1
+    if tm == 30:
+        tm = 0
+        summa += cl1
     pygame.display.flip()
